5b03597c131acf1bc46764efa0510b464577f922
League Student <league@iMac-14.attlocal.net>
2020-01-05 15:57:56 -0800

    houses

--- src/_01_houses/Houses.java
+++ src/_01_houses/Houses.java
@@ -1,7 +1,14 @@
 package _01_houses;
 
+import org.jointheleague.graphical.robot.Robot;
+
 public class Houses {
 	public void run() {
-		
+		Robot rob = new Robot();
+		rob.setX(0);
+		rob.setY(0);
+		for(int i=0; i<3; i++) {
+			rob.move(100);
+		}
 	}
 }
